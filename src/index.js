1e351a0afc4833c1264e48d08ce3f3cedb2b7e11
Obed Ampah <oampah@gmail.com>
2017-08-02 17:12:53 -0400

    Adding ESLint to the project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<div>'Wunderbar,' He exclaimed with great relish.</div>, document.getElementById('root'))
+ReactDOM.render(<div>Wunderbar, He exclaimed with great relish.</div>, document.getElementById('root'))
