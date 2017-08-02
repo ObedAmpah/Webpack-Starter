cbfc00f652dc1543d7a50a345a765d8889a73f3c
Obed Ampah <oampah@gmail.com>
2017-08-02 14:46:48 -0400

    Starting to develop the file-chunking of my starter

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,6 +3,7 @@ import path from 'path'
 import merge from 'webpack-merge'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import CleanWebpackPlugin from 'clean-webpack-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 
 // Define our directory paths
@@ -24,15 +25,33 @@ const plugins = [
     dry: false,
     watch: true
   }),
+
   new HtmlWebpackPlugin({
     inject: true,
     template: path.join(paths.appRoot, '/src/index.html')
+  }),
+
+  new webpack.optimize.UglifyJsPlugin({
+    sourceMap: true
+  }),
+
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'node-static',
+    filename: 'node-static.js',
+    minChunks(module, count) {
+      const context = module.context
+      return context && context.indexOf('node_modules') >= 0
+    }
+  }),
+
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static'
   })
 ]
 
 const config = {
   context: paths.appRoot,
-  devtool: 'inline-source-map',
+  devtool: 'eval',
   entry: entries,
 
   // Define our ouput point
